956545c26a43f5afdd4c2e2e7b344d2fce33f180
puhrome <phirom.yim@fullsail.edu>
2014-05-15 16:03:11 -0400

    Modulo Arithmetic Operator
    Using the % modulo to calculate the remainder in an expression in JS
    through WebStorm

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Modulo Expressions Created by Phirom Yim on 5/15/14.
+ */
+
+var remainder = 32/10;//will divide 32 by 10
+console.log(remainder);//will calculate the equation without remainder
+//% to get remainder
+
+var remainder = 32%10;//use modulo operator
+console.log(remainder);//gives the remainder amount
